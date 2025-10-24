fe13187dcd5a43296d256db5f478bf4f99a2086c
Kamil <kulikkamil@gmail.com>
2025-10-24 12:41:59 +0200

    add neon serverless adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,11 +7,29 @@ import {
   User,
 } from "@prisma/client";
 
+import "dotenv/config";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
+
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+neonConfig.poolQueryViaFetch = true;
+
+// Type definitions
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.VERCEL_ENV !== "production") globalForPrisma.prisma = prisma;
 
