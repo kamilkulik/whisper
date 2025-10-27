2e57513e9eb64d4ec24cdbdae6a1dcb6279a4c85
Kamil <kulikkamil@gmail.com>
2025-10-27 07:02:13 +0100

    skeleton for legal pages

---
+++ src/app/(legal)/terms/page.tsx
@@ -0,0 +1,10 @@
+// server component
+import { ReturnButton } from "../../_components/ReturnButton";
+
+export default async function TermsAndConditionsPage() {
+  return (
+    <div className="flex flex-col items-center mb-3">
+      <ReturnButton href="/" />
+    </div>
+  );
+}
